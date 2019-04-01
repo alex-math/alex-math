745b7513b3015e8ddf122279a5e9726f5dc05837
Jean Talu <jean.taluminium@gmail.com>
2019-04-01 17:18:34 +0200

    Bouton aller-retour OK

---
+++ js/jquery.buttonLoader.mini.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    
+    $('.has-spinner').click(function () {
+        var btn = $(this);
+        $(btn).buttonLoader('start');
+        setTimeout(function () {
+            $(btn).buttonLoader('stop');
+        }, 5000);
+    });
+});
